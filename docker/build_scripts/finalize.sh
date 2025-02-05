974bbb9ae789090dd63d874c2f976bcfb3f6daa5
mayeut <mayeut@users.noreply.github.com>
2025-02-05 06:53:05 +0100

    Update finalize.sh

--- docker/build_scripts/finalize.sh
+++ docker/build_scripts/finalize.sh
@@ -87,7 +87,7 @@ for TOOL_PATH in "${MY_DIR}/requirements-tools/"*; do
 		musllinux*_ppc64le-uv) continue;;  # uv doesn't provide musl ppc64le wheels due to Rust issues
 		musllinux*_s390x-uv) continue;;  # uv doesn't provide musl s390x wheels due to Rust issues
 		*_riscv64-uv) continue;;  # no uv for riscv64
-		musllinux*_riscv64-cmake|musllinux*_riscv64-swig) apk --no-cache add ${TOOL};;
+		musllinux*_riscv64-cmake|musllinux*_riscv64-swig) apk --no-cache add "${TOOL}";;
     musllinux*_riscv64-patchelf)
 			apk --no-cache add cmake
 			pipx install patchelf==0.17.2.1
